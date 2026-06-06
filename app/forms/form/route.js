1e50aabdcee38e0c9852af569ba3c24e3eab7789
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-06-06 06:58:13 +0530

    Created form backend

--- app/forms/form/route.js
+++ app/forms/form/route.js
@@ -1,6 +1,29 @@
 import { NextResponse } from "next/server";
+import mongoose from 'mongoose'
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(process.env.DBLINK);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+};
+const datachema=new mongoose.Schema({
+    appname:String,
+    user:String
+})
+const data=mongoose.model('data',datachema)
+
 export  async function POST(req){
+    await connectToDB();
     const {appname,user}=await req.json();
     console.log(appname + user);
+    let d=new data({
+        appname:appname,
+        user:user
+    })
+    await d.save().then(()=>{console.log("Data Saved!!");
+    }).catch((err)=>{console.error(err)});
     return NextResponse.json({message:"Data received successfully"});
 }
